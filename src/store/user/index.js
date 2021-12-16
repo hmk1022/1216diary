82a10542e4ea04216ef6d36f3c3a4785f7f97926
hmk1022 <hm.kim@iworkerman.com>
2021-12-16 09:29:39 +0900

    first commit

---
+++ src/store/user/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-alert, no-console */
+import { getters } from "./getters";
+import { actions } from "./actions";
+import { mutations } from "./mutations";
+
+const state = {
+
+};
+
+export const user = {
+  state,
+  getters,
+  actions,
+  mutations,
+};
+
+/* eslint-enable no-alert */
